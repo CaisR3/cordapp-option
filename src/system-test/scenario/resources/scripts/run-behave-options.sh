ef8f9ef928d94806918b5b92f397cfea1df0e499
josecoll <jose.coll@r3cev.com>
2018-06-13 17:58:44 +0100

    Updated for Corda Enteprise GA 3.0 Release Candidate testing.

--- src/system-test/scenario/resources/scripts/run-behave-options.sh
+++ src/system-test/scenario/resources/scripts/run-behave-options.sh
@@ -6,12 +6,34 @@
 # $ pwd
 # ./IdeaProjects/cordapp-option
 # $ src/system-test/scenario/resources/scripts/run-behave-options.sh
+#
+# NOTE: remember to setup your staging environment using the `prepare` script of gradle task.
+#
 
-CURRENT_DIR=$PWD
+SAMPLE_HOME=$PWD
+BEHAVE_DIR=${SAMPLE_HOME}/../../experimental/behave
 
-BEHAVE_DIR=/Users/josecoll/IdeaProjects/corda-reviews/experimental/behave/build/libs
+cd ${BEHAVE_DIR}
+BEHAVE_JAR=$(ls ${BEHAVE_DIR}/build/libs/corda-behave-[0-9]*.jar | tail -n1)
 
-./gradlew scenarioJar
+if [ ! -f "${BEHAVE_JAR}" ]; then
+    echo "Building behaveJar ..."
+    ../../gradlew behaveJar
+fi
+BEHAVE_JAR=$(ls ${BEHAVE_DIR}/build/libs/corda-behave-[0-9]*.jar | tail -n1)
 
-java -cp "$BEHAVE_DIR/corda-behave.jar:$CURRENT_DIR/build/libs/cordapp-option-behave-test.jar" net.corda.behave.scenarios.ScenarioRunner --glue net.corda.behave.scenarios -path ./src/system-test/scenario/resources/features/options.feature -d
+
+cd ${SAMPLE_HOME}
+SAMPLE_JAR=$(ls ${SAMPLE_HOME}/build/libs/cordapp-option-*.jar | tail -n1)
+if [ ! -f "${SAMPLE_JAR}" ]; then
+    echo "Building scenarioJar ..."
+    ./gradlew scenarioJar
+fi
+SAMPLE_JAR=$(ls ${SAMPLE_HOME}/build/libs/cordapp-option-*.jar | tail -n1)
+
+# QA interoperability
+CMD="java -DSTAGING_ROOT=${STAGING_ROOT} -DDISABLE_CLEANUP=true -cp ${BEHAVE_JAR}:${SAMPLE_JAR} net.corda.behave.scenarios.ScenarioRunner -path ${SAMPLE_HOME}/src/system-test/scenario/resources/features/options.feature"
+# -d to perform dry-run
+echo "Executing: ${CMD}"
+eval `${CMD}`
 # -d to perform dry-run
